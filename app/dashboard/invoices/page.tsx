ca89bdaaeb1d7a8f3f406541a0ce049ff0120a4e
최정민(jungmin_choi1) <jungmin_choi1@kolon.com>
2024-03-20 10:28:51 +0900

    Initial commit

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,11 @@
 const InvoicesPage = () => {
-  return <p>Invoices Page</p>;
+  return (
+    <p>
+      Invoices Page
+      <input id="rn" name="asdf" required />
+      <label htmlFor="rn">asdf</label>
+    </p>
+  );
 };
 
 export default InvoicesPage;
